0f8d2c0c8256231ec0d6fbed79daef1662fce231
VictoryChibueze <alhajivicci12@gmail.com>
2024-11-15 10:35:31 +0100

    web341

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const indexRoute = require("./routes/index");
 
+app.use("/", indexRoute);
 const Port = process.env.PORT || 8080;
 app.listen(Port, () => {
   console.log(`App is listening on ${Port}`);
