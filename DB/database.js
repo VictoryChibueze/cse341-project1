37319f63f2e2bfc868b695f91a84c141b04e56fb
VictoryChibueze <alhajivicci12@gmail.com>
2024-11-15 13:37:01 +0100

    web341

--- DB/database.js
+++ DB/database.js
@@ -1,2 +1,23 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
+const MongoClient = require("mongodb").mongoClient;
+
+let database;
+
+const initDb = (callback) => {
+  if (database) {
+    console.log(`Database is already initialized`);
+    return callback(null, callback);
+  }
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      database = client;
+      callback(null, database);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getData = () => {};
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const mongodb = require("./DB/database");
 const indexRoute = require("./routes/index");
 
 app.use("/", indexRoute);
 const Port = process.env.PORT || 8080;
-app.listen(Port, () => {
-  console.log(`App is listening on ${Port}`);
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(Port, () => {
+      console.log(`App is listening on ${Port}`);
+    });
+  }
 });
