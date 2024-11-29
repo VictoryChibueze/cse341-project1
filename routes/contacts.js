d3d4efec4236155a37b804239a8e20d88d5ee060
VictoryChibueze <alhajivicci12@gmail.com>
2024-11-29 10:40:08 +0100

    .

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const contactController = require("../controllers/contactController");
+const validation = require("../middleware/validate");
 
 router.get("/", contactController.getAll);
 router.get("/:id", contactController.getSingle);
-router.post("/", contactController.createContact);
-router.put("/:id", contactController.updateContact);
+router.post("/", validation.saveContact, contactController.createContact);
+router.put("/:id", validation.saveContact, contactController.updateContact);
 router.delete("/:id", contactController.deleteContact);
 module.exports = router;
